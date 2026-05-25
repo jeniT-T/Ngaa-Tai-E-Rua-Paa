55dabb3aa43fc7768289bf9929961739cf873d78
jeniT-T <jennyee.nz@gmail.com>
2026-05-26 02:59:01 +1200

    Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,22 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+      {/*Logo */}
+      <div 
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "12px",
+      }}>
+      <img src="/images/logo.png"
+      alt="Marae Logo"
+      style={{
+        width: "45px",
+        height: "45px",
+        objectFit: "contain",
+      }}/>
       <h3>Marae System</h3>
+      </div>
 
       <div style={{ display: "flex", gap: "15px" }}>
         <Link to="/">Home</Link>
