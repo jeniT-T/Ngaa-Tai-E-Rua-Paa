a8d47ed05e73ecb3f1e45ff97e32e14407e88bda
jeniT-T <jennyee.nz@gmail.com>
2026-05-26 04:51:17 +1200

    layout and hover interaction navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,31 +2,23 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-      {/*Logo */}
-      <div 
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: "12px",
-      }}>
+    <header>
+      {/*Logo and Title*/}
+      <div className="header">
       <img src="/images/logo.png"
       alt="Marae Logo"
-      style={{
-        width: "45px",
-        height: "45px",
-        objectFit: "contain",
-      }}/>
+      className="logo"
+      />
       <h3>Marae System</h3>
       </div>
-
-      <div style={{ display: "flex", gap: "15px" }}>
+      {/*Navigation bar*/}
+      <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/arrival">Arrival Info</Link>
         <Link to="/contacts">Contacts</Link>
         <Link to="/health-and-safety">Health & Safety</Link>
-      </div>
     </nav>
+    </header>
   );
 }
 
